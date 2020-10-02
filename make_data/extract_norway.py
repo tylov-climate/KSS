eaca980b53feb6d4ee48c87a29ae4b9fc715cd83
tylo <tylo@norceresearch.no>
2020-10-02 16:49:25 +0200

    More cleanup

--- make_data/extract_norway.py
+++ make_data/extract_norway.py
@@ -14,23 +14,35 @@ eur_rpole = ((-28.375, -23.375), (18.155, 21.835)) # lon-lat
 nor_rpole = ((-6.595, 7.535), (4.735, 20.625))     # lon-lat
 nor_lonlat = ((5.684921607269574, 57.73557430824275), (31.682012795900178, 70.93623338069854))
 
+# Labbert conformal: 453 x 453 grid
+# CNRM/CNRM-CERFACS-CNRM-CM5/*/r1i1p1/ALADIN63/v2
+# CNRM/CNRM-CERFACS-CNRM-CM5/*/r1i1p1/ALADIN53/v1
+# CNRM/MPI-M-MPI-ESM-LR/(hist,rcp85)/r1i1p1/ALADIN63/v2
+# CNRM/MOHC-HadGEM2-ES/(hist,rcp85)/r1i1p1/ALADIN63/v1
+
+# Lambert conformal: 485 x 485 grid
+# RMIB-UGent/CNRM-CERFACS-CNRM-CM5/*/r1i1p1/ALARO-0
+
+# Lambert conformal: 527 x 527: crs:proj4_params = "+proj=lcc +lat_1=30.00 +lat_2=65.00 +lat_0=48.00 +lon_0=9.75 +x_0=-6000. +y_0=-6000. +ellps=sphere +a=6371229. +b=6371229. +units=m +no_defs" ;
+# ICTP/MPI-M-MPI-ESM-LR/(hist,rcp26,rcp85)/r1i1p1/RegCM4-6/v1
+# ICTP/MOHC-HadGEM2-ES/(hist,rcp26,rcp85)/r1i1p1/RegCM4-6/v1
 
 class Projection:
     def __init__(self):
         self.pcarr = cartopy.crs.PlateCarree()
         self.rpole = cartopy.crs.RotatedPole(pole_latitude=39.25, pole_longitude=-162.0)
 
-    def convert_to_lonlat(self, p):
+    def rotpole_to_lonlat(self, p):
         return self.pcarr.transform_point(p[0], p[1], self.rpole)
 
-    def convert_to_rotpole(self, p):
+    def lonlat_to_rotpole(self, p):
         return self.rpole.transform_point(p[0], p[1], self.pcarr)
 
 
 def crop_cordex_eur11_to_norway(inroot, outroot):
     proj = Projection()
-    p = proj.convert_to_rotpole(nor_lonlat[0])
-    q = proj.convert_to_rotpole(nor_lonlat[1])
+    p = proj.lonlat_to_rotpole(nor_lonlat[0])
+    q = proj.lonlat_to_rotpole(nor_lonlat[1])
     print('Rotated pole lon:',(p[0],q[0]), 'lat:',(p[1],q[1]))
 
     # Rather use absolute pixels p=lower left, q=upper right
@@ -48,26 +60,31 @@ def crop_cordex_eur11_to_norway(inroot, outroot):
                     subpath = root.replace(inroot, '')
                     outdir = os.path.join(outroot, subpath)
                     outfile = os.path.join(outdir, f)
-                    print('Path:', subpath)
-                    print('     ', f)
                     if not os.path.isfile(outfile):
                         # Crop bounding box of Norway
-
                         #ret = os.system('ncks -d rlon,%f,%f -d rlat,%f,%f %s -O %s'
-                        ret = os.system('ncks -d rlon,%d,%d -d rlat,%d,%d %s -O %s'
-                                         % (p[0],q[0], p[1],q[1], infile, 'tmp.nc'))
+                        print('Path:', subpath, file=sys.stderr)
+                        print('     ', f, file=sys.stderr)
+                        #ret = os.system('ncks -d rlon,%d,%d -d rlat,%d,%d %s -O %s'
+                        #                 % (p[0],q[0], p[1],q[1], infile, 'tmp.nc'))
+                        ret = -1
+                        if ret != 0:
+                            ret = os.system('cdo remapbil,griddes.txt %s %s' % (infile, 'tmp.nc'))
+                            
                         if ret == 0:
                             if not os.path.isdir(outdir):
                                 os.makedirs(outdir)
                             os.rename('tmp.nc', outfile)
-                        print('Result:', ret)
+                        else:
+                            print('Result:', ret, file=sys.stderr)
                     else:
-                        print('Exists')
+                        pass
+                        #print('Exists')
 
 def sample_test():
     proj = Projection()
-    p = proj.convert_to_rotpole(nor_lonlat[0])
-    q = proj.convert_to_rotpole(nor_lonlat[1])
+    p = proj.lonlat_to_rotpole(nor_lonlat[0])
+    q = proj.lonlat_to_rotpoley(nor_lonlat[1])
     print('lon',(p[0],q[0]), 'lat',(p[1],q[1]))
     samples = os.path.join('..', 'sample_data')
     base = '_EUR-11_ICHEC-EC-EARTH_rcp85_r12i1p1_SMHI-RCA4_v1_day_20060101-20101231.nc'
---
+++ make_plots/plots.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def add_last_study_column(df):
+    original_models = np.array([
+        'ICHEC-EC-EARTH_HIRHAM5_r3i1p1',
+        'ICHEC-EC-EARTH_RCA4_r12i1p1',
+        'ICHEC-EC-EARTH_CCLM4-8-17_r12i1p1',
+        'ICHEC-EC-EARTH_RACMO22E_r1i1p1',
+        'CNRM-CERFACS-CNRM-CM5_RCA4_r1i1p1',
+        'CNRM-CERFACS-CNRM-CM5_CCLM4-8-17_r1i1p1',
+        'IPSL-IPSL-CM5A-MR_RCA4_r1i1p1',
+        'IPSL-IPSL-CM5A-MR_WRF331F_r1i1p1',
+        'MPI-M-MPI-ESM-LR_REMO2009_r1i1p1',
+        'MPI-M-MPI-ESM-LR_RCA4_r1i1p1',
+        'MPI-M-MPI-ESM-LR_CCLM4-8-17_r1i1p1',
+        'MOHC-HadGEM2-ES_RCA4_r1i1p1',
+    ])
+
+    #sns.set_style("ticks")
+    sns.set_style('whitegrid')
+    
+    full_mods = df[df.columns[4:7]].apply(
+        lambda x: '_'.join(x.dropna().astype(str)),
+        axis=1
+    )
+    last_study = full_mods == original_models[0]
+    for i in range(1, len(original_models)):
+        last_study |= full_mods == original_models[i]
+    df['Last Study'] = last_study
+    
+    #for i in range(len(last_study)):
+    #    print(full_mods[i], last_study[i])
+
+    #comb_period = df[df.columns[1:3]].apply(
+    #    lambda x: '_'.join(x.dropna().astype(str)),
+    #    axis=1
+    #)    
+    #df['Combined Period'] = comb_period
+
+
+
+def plot1(df):
+    markers = ['o', 'v', 'd', '^', 'D', ]  # '^', 'D', 'v', 'o', 'd', 'v', 'v', 'v', '^', 'v']
+    #df[df.Season == 'spring']
+    g = sns.FacetGrid(df, col='Season', row='Period', hue='Experiment', palette="bright") # height=6, aspect=.8
+    g.map_dataframe(sns.scatterplot, x='PR mm.day', y='TAS celsius', style='Last Study', markers=markers)
+    g.set_axis_labels('Precipitation mm.', 'Surface temp. C.')
+    g.add_legend()
+    #g.set_titles(col_template="{col_name} patrons", row_template="{row_name}")
+    g.set(xlim=(1.6, 4.4), ylim=(-10, 19)) # , xticks=[10, 30, 50], yticks=[2, 6, 10])
+    #g.set(xlim=(1.6, 4.4), ylim=(8, 19)) # , xticks=[10, 30, 50], yticks=[2, 6, 10])
+    #g.savefig("facet_plot.png")
+    plt.show()
+
+
+def plot2(df):
+    markers = ('o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X')
+    #df[df.Season == 'spring']
+    g = sns.FacetGrid(df[df.Season == 'annual'], row='Last Study', col='Period', hue='Experiment', palette="bright") # height=6, aspect=.8
+    g.map_dataframe(sns.scatterplot, x='PR mm.day', y='TAS celsius', style='Institute', markers=markers, legend='full')
+    g.set_axis_labels('Precipitation mm.', 'Surface temp. C.')
+    g.add_legend()
+    #g.set_titles(col_template="{col_name} patrons", row_template="{row_name}")
+    g.set(xlim=(2, 3.8), ylim=(-3, 9)) # , xticks=[10, 30, 50], yticks=[2, 6, 10])
+    #g.set(xlim=(1.6, 4.4), ylim=(8, 19)) # , xticks=[10, 30, 50], yticks=[2, 6, 10])
+    #g.savefig("facet_plot.png")
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    # Read dataset
+    df = pd.read_csv('kss_analysis.csv', index_col=0)
+    #df = pd.read_pickle('kss_analysis.pkl')
+    #print(df.head())
+    add_last_study_column(df)
+    plot2(df)
