25b1a7152e07b36e1c8b505a35e50a05d5b4735f
tylo <tylo@norceresearch.no>
2022-11-29 11:48:12 +0100

    Updated paths etc.

--- make_data/senorge2018_cmorize.sh
+++ make_data/senorge2018_cmorize.sh
@@ -1,12 +1,23 @@
 #/bin/bash
-cdo selvar,rr seNorge2018_1951-2000.nc tmp1.nc
-ncrename -O -v rr,pr tmp1.nc tmp2.nc
-ncap2 -O -s "pr=pr/86400" tmp2.nc tmp1.nc
-ncatted -O -a units,pr,o,c,"kg m-2 s-1" -a standard_name,pr,o,c,"precipitation_flux" -a long_name,pr,o,c,"Precipitation" tmp1.nc ../stats_v3/yseasmean/pr_yseasmean_1951-2000_histo/pr_EUR-11_seNorge_seNorge2018_historical_r1i1p1_None_v1_yseasmean_v20190101_1951-2000.nc
+input=/nird/projects/NS9001K/tyge/seNorge2018
+out=../..seNorge2018_cmor
+mkdir -p $out
 
-cdo selvar,tg seNorge2018_1951-2000.nc tmp1.nc
-ncrename -O -v tg,tas tmp1.nc tmp2.nc
-ncap2 -O -s "tas=tas+273.15" tmp2.nc tmp1.nc
-ncatted -O -a units,tas,o,c,"K" -a long_name,tas,o,c,"Near-Surface Air Temperature" tmp1.nc ../stats_v3/yseasmean/tas_yseasmean_1951-2000_histo/tas_EUR-11_seNorge_seNorge2018_historical_r1i1p1_None_v1_yseasmean_v20190101_1951-2000.nc
+for i in $(seq $1 $2); do
+    cdo remapbil,griddes.txt $input/seNorge2018_$i.nc seNorge2018_${i}_remap.nc
 
-rm tmp1.nc tmp2.nc
+    cdo selvar,rr seNorge2018_${i}_remap.nc pr1_$i.nc
+    ncrename -O -v rr,pr pr1_$i.nc pr2_$i.nc
+    ncap2 -O -s "pr=pr/86400" pr2_$i.nc pr1_$i.nc
+    ncatted -O -a units,pr,o,c,"kg m-2 s-1" -a standard_name,pr,o,c,"precipitation_flux" -a long_name,pr,o,c,"Precipitation" pr1_$i.nc  $out/pr_seNorge2018_${i}_remap.nc
+    #  ../stats_v3/yseasmean/pr_yseasmean_1951-2000_histo/pr_EUR-11_seNorge_seNorge2018_historical_r1i1p1_None_v1_yseasmean_v20190101_1951-2000.nc
+    rm pr1_$i.nc pr2_$i.nc
+
+    cdo selvar,tg seNorge2018_${i}_remap.nc tas1_$i.nc
+    ncrename -O -v tg,tas tas1_$i.nc tas2_$i.nc
+    ncap2 -O -s "tas=tas+273.15" tas2_$i.nc tas1_$i.nc
+    ncatted -O -a units,tas,o,c,"K" -a long_name,tas,o,c,"Near-Surface Air Temperature" tas1_$i.nc  $out/tas_seNorge2018_${i}_remap.nc
+    # ../stats_v3/yseasmean/tas_yseasmean_1951-2000_histo/tas_EUR-11_seNorge_seNorge2018_historical_r1i1p1_None_v1_yseasmean_v20190101_1951-2000.nc
+    rm tas1_$i.nc tas2_$i.nc
+    rm seNorge2018_${i}_remap.nc
+done
