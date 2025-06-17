eb2bd5da2e1f3e686eee2fe741f1286b19a798c9
tylo <tylo@norceresearch.no>
2025-06-17 23:14:24 +0200

    Added some scripts

---
+++ make_data/run6.sh
@@ -0,0 +1,1 @@
+python make_cmip6_stats.py --ensemble -s mean --interval ymon -t KNMI
---
+++ make_plots/run.sh
@@ -0,0 +1,31 @@
+
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v PR -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save --all
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v PR -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v PR -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s MAM
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v PR -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s JJA
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v PR -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s SON
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v PR -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s DJF
+
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v TAS -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save --all
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v TAS -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v TAS -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s MAM
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v TAS -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s JJA
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v TAS -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s SON
+python plot_kss.py -f yseasmean_cmip5.csv -P bar -v TAS -p 2 -d 'rcp45-4' -o ~/kss-plots-2025 --save -s DJF
+
+
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P scatter -v TAS -p 2 -d 'rcp45-4' --all
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P scatter -v PR -p 2 -d 'rcp45-4' --all
+
+
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v TAS -p 2 -d 'rcp45-4'
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v TAS -p 2 -d 'rcp45-4' -s MAM
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v TAS -p 2 -d 'rcp45-4' -s JJA
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v TAS -p 2 -d 'rcp45-4' -s SON
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v TAS -p 2 -d 'rcp45-4' -s DJF
+
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v PR -p 2 -d 'rcp45-4'
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v PR -p 2 -d 'rcp45-4' -s MAM
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v PR -p 2 -d 'rcp45-4' -s JJA
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v PR -p 2 -d 'rcp45-4' -s SON
+python plot_kss.py -f yseasmean_cmip5.csv -o ~/kss-plots-2025 --save -P kdediff -v PR -p 2 -d 'rcp45-4' -s DJF
